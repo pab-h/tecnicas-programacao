080dac9f15fac72f4a073abcd9379ad960df424a
pab-h <dev.pab.2020@gmail.com>
2023-06-26 21:54:39 -0300

    Mensagem de erro no clique da celula sem selecionar o aspirador

--- trabalho2/src/apresentacao/tabuleiro/CliqueNaCelula.java
+++ trabalho2/src/apresentacao/tabuleiro/CliqueNaCelula.java
@@ -3,6 +3,8 @@ package apresentacao.tabuleiro;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 import apresentacao.Icone;
 
 import controle.Gerenciador;
@@ -21,10 +23,23 @@ public class CliqueNaCelula implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		AspiradorVirtual aspiradorVirtual = gerenciador.getAspiradorVirtualSelecionado();
 		
+		if (aspiradorVirtual == null) {
+			JOptionPane.showMessageDialog(
+				null, 
+				"Selecione um aspirador para posicionar", 
+				"Erro!", 
+				JOptionPane.ERROR_MESSAGE
+			);
+			
+			return;
+		}
+		
 		aspiradorVirtual.setCelulaVirtualAtual(this.celula.getCelulaVirtual());
 		this.celula.getCelulaVirtual().setAspiradorVirtual(aspiradorVirtual);
+		
 		Icone iconeAspiradorVirtual = new Icone(aspiradorVirtual.getImagem());
 		this.celula.setIcon(iconeAspiradorVirtual.getIcon(50, 50));
+		
 		this.gerenciador.setAspiradorVirtualSelecionado(null);
 	}
 
